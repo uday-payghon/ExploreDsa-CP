936c586b47c62c1104f03197416267904027032c
Uday Payghon <udaypayghon@gmail.com>
2026-02-26 16:12:43 +0530

    Data push

---
+++ src/L22and23_STRINGs/Hello.java
@@ -0,0 +1,10 @@
+ package L22and23_STRINGs;
+ import java.util.*;
+
+public class Hello
+{
+    public static void main(String args[])
+    {
+        System.out.println("SYSTEM WILL HERE");
+    }
+}
