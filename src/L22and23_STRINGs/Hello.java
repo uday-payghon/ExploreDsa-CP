9a87765bf4536cc3f95b7898483340623e14d3a7
Uday Payghon <udaypayghon@gmail.com>
2026-02-26 16:32:48 +0530

    Start String

--- src/L22and23_STRINGs/Hello.java
+++ src/L22and23_STRINGs/Hello.java
@@ -5,6 +5,6 @@ public class Hello
 {
     public static void main(String args[])
     {
-        System.out.println("SYSTEM WILL HERE");
+        System.out.println("SYSTEM WILL Hacked hereHERE");
     }
 }
---
+++ src/L22and23_STRINGs/New.java
@@ -0,0 +1,4 @@
+package L22and23_STRINGs;
+
+public class New {
+}
